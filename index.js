81c7a2d358ba78846f7ff914c52489acd5315e1f
COMRADE040 <rafiulislam040@gmail.com>
2025-10-26 23:15:10 +0600

    practicing with json file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 3200;
 
@@ -7,8 +8,15 @@ app.get('/', (req, res) => {
     res.send('My Full Info are Coming Soon')
 })
 
-app.get('/first', (req, res) => {
-    res.send('Okay Lets See')
+app.get('/phones', (req, res) => {
+    req.send(phones)
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id: ', id)
+    const phone = phones.find(phone => phone.id === id) || {}
+    res.send(phone)
 })
 
 app.listen(port, () => {
