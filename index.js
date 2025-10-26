677293f152b8001a75b1c2d7730ce995b0c4d9f3
COMRADE040 <rafiulislam040@gmail.com>
2025-10-27 00:07:01 +0600

    succesfully know

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const phones = require('./phones.json');
 const app = express();
-const port = 3200;
+const port = 5000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('My Full Info are Coming Soon')
-})
+    res.send('my phones server coming soon toon moon')
+});
 
 app.get('/phones', (req, res) => {
-    req.send(phones)
+    res.send(phones);
 })
 
 app.get('/phones/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    console.log('i need data for id: ', id)
-    const phone = phones.find(phone => phone.id === id) || {}
-    res.send(phone)
+    console.log('i need data for id: ', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
 })
 
 app.listen(port, () => {
-    console.log(`My Phone server is Running on port: ${port}`)
+    console.log(`My phone server is running on port: ${port}`);
 })
